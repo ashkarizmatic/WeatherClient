4b3b3593c30b7b8a007f54fb7e9951527a0e6b89
amahale <mahale.ashish@gmail.com>
2016-01-10 17:40:10 -0800

    Final commit of the Weather client from CDYNE Weather service. The project is packaged as a jar using Maven.

---
+++ WeatherClient/src/main/java/com/ashishmahale/api/weatherClient/Demo.java
@@ -0,0 +1,26 @@
+package com.ashishmahale.api.weatherClient;
+
+public class Demo {
+	
+/*	  public static void main( String[] args )
+	    {
+	        Weather service = new Weather();
+	        WeatherSoap weatherSoap = service.getWeatherSoap();
+	        WeatherReturn weather = weatherSoap.getCityWeatherByZIP("98042");
+	        
+	        StringBuilder sb = null;
+	        
+	        if(weather.isSuccess()) {
+	        	sb = new StringBuilder();
+	        	sb.append("The current weather for " +weather.getCity()+ " is "+weather.getDescription()+ " skies with a temperature of "+weather.getTemperature()+" degrees farenheit.");
+	        	System.out.println(sb.toString());
+	        	
+	        }
+	        else {
+	        	System.out.println("Could not fetch weather information");
+	        }
+	        
+	        
+	    }*/
+
+}
